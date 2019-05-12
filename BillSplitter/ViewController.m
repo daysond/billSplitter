3e510dfe74a388732b9733e00f83a10e6caceae5
Dayson Dong <daysond.yd@gmail.com>
2019-05-12 14:56:28 -0400

    Before Removing Split Bill Button

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextfield;
+@property (weak, nonatomic) IBOutlet UISlider *personSlider;
+@property (weak, nonatomic) IBOutlet UILabel *billAmountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *personNumLabel;
+@property (nonatomic) NSDecimalNumber* totalBillAmount;
+@property (nonatomic) NSDecimalNumber* billAmount;
+@property (nonatomic) NSDecimalNumber* numberOfPerson;
 
 @end
 
@@ -16,8 +23,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.personSlider.value = 2.0;
+    self.numberOfPerson = [NSDecimalNumber decimalNumberWithString:@"2.0"];
+    _personNumLabel.text = [NSString stringWithFormat:@"Number Of Person: %@",[[NSNumberFormatter new] stringFromNumber:self.numberOfPerson]] ;
+    _billAmountLabel.text = @"";
 }
+- (IBAction)sliderValueChanged:(UISlider *)sender {
+    self.numberOfPerson = [[NSDecimalNumber alloc]initWithInt:(int)sender.value];
+     _personNumLabel.text = [NSString stringWithFormat:@"Number Of Person: %@",[[NSNumberFormatter new] stringFromNumber:self.numberOfPerson]] ;
+}
+- (IBAction)calculateSplitAmount:(id)sender {
+    
+    NSDecimalNumberHandler *behavior = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundBankers scale:2 raiseOnExactness:NO raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:NO];
+    
+    self.totalBillAmount = [NSDecimalNumber decimalNumberWithString:_billAmountTextfield.text];
+    NSLog(@"total %@",self.totalBillAmount);
+    self.billAmount = [self.totalBillAmount decimalNumberByDividingBy:self.numberOfPerson];
+    NSLog(@"bill %@",self.billAmount);
+    _billAmountLabel.text = [NSString stringWithFormat:@"Bill Amount splitted by %@ is $%@ ",self.numberOfPerson,[self.billAmount decimalNumberByRoundingAccordingToBehavior:behavior]] ;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    
+    
+    
+}
+
+
+
+
 
 
 @end
